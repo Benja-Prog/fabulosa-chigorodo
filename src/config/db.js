6155cfc957bce97c56149b10e690dcfb2660887a
Benja-Prog <rey-colombia@hotmail.com>
2022-04-06 18:00:56 -0500

    db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,17 @@
 var mysql = require('mysql');
 
-exports.connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: localhost,
     user: lafabulo_root,
     password: Kan533846,
     database: lafabulo_db_fabulosa
 });
 
-exports.connection.connect((error)=>{
+connection.connect((error)=>{
     if(error){
         console.log('El error de conexión es : ' +error);
         return;
     }
     console.log('Conectado Exitosamente a Base de Datos');
 })
+module.exports = connection;
