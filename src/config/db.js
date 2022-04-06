e65f0187aa5485bec6f6dfc54f209266ebf4d9d1
Benja-Prog <rey-colombia@hotmail.com>
2022-04-06 17:27:23 -0500

    Cambio de parametros en db,js

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: localhost,
+    user: lafabulo_root,
+    password: Kan533846,
+    database: lafabulo_db_fabulosa
 });
 
 connection.connect((error)=>{
